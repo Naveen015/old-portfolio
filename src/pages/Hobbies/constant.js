37894743c8fc7eb9743ce5d7a2994a81ebd4c44d
Naveen Prashanna G <35716708+Naveen015@users.noreply.github.com>
2023-08-31 12:09:13 -0500

    Update constant.js

--- src/pages/Hobbies/constant.js
+++ src/pages/Hobbies/constant.js
@@ -1,18 +1,7 @@
-import eye from "@assets/Art/eye.jpg";
-import carizard from "@assets/Art/carizard.jpg";
-import dragon from "@assets/Art/dragon.jpg";
-import goku from "@assets/Art/goku.jpg";
-import ichigo from "@assets/Art/ichigo.jpg";
-import kenaki from "@assets/Art/kenaki.jpg";
-import minato from "@assets/Art/minato.jpg";
-import naruto from "@assets/Art/naruto.jpg";
-import natsu from "@assets/Art/natsu.jpg";
-import parrot from "@assets/Art/parrot.jpg";
-import solo from "@assets/Art/solo.png";
-import stone from "@assets/Art/stone.jpg";
-import sword_art from "@assets/Art/sword_art.jpg";
-import thread from "@assets/Art/thread.jpg";
-import sword from "@assets/Art/sword.jpg";
+import frisbee from "@assets/Art/naruto.jpg";
+import football from "@assets/Art/parrot.jpg";
+import trekking from "@assets/Art/solo.png";
+import marathon from "@assets/Art/sword.jpg";
 
 const groupCardConfig = {
   xs: "1",
@@ -24,14 +13,14 @@ const artData = [
   {
     cardConfig: {},
     isVideo: false,
-    src: solo,
+    src: trekking,
     srcConfig: {
       variant: "top",
       style: { display: "block", height: "100%", width: "100%" },
     },
     header: "",
     headerConfig: {},
-    title: "",
+    title: "Trekking",
     body: "",
     footer: "",
     footerConfig: { className: "text-muted d-flex justify-content-end" },
@@ -39,14 +28,14 @@ const artData = [
   {
     cardConfig: {},
     isVideo: false,
-    src: sword,
+    src: marathon,
     srcConfig: {
       variant: "top",
       style: { display: "block", height: "100%", width: "100%" },
     },
     header: "",
     headerConfig: {},
-    title: "",
+    title: "Marathon",
     body: "",
     footer: "",
     footerConfig: { className: "text-muted d-flex justify-content-end" },
@@ -54,14 +43,14 @@ const artData = [
   {
     cardConfig: {},
     isVideo: false,
-    src: naruto,
+    src: frisbee,
     srcConfig: {
       variant: "top",
       style: { display: "block", height: "100%", width: "100%" },
     },
     header: "",
     headerConfig: {},
-    title: "",
+    title: "Frisbee",
     body: "",
     footer: "",
     footerConfig: { className: "text-muted d-flex justify-content-end" },
@@ -69,183 +58,18 @@ const artData = [
   {
     cardConfig: {},
     isVideo: false,
-    src: parrot,
+    src: football,
     srcConfig: {
       variant: "top",
       style: { display: "block", height: "100%", width: "100%" },
     },
     header: "",
     headerConfig: {},
-    title: "",
+    title: "Football",
     body: "",
     footer: "",
     footerConfig: { className: "text-muted d-flex justify-content-end" },
   },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: minato,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: dragon,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: stone,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: kenaki,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: eye,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: natsu,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: thread,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: carizard,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: goku,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: ichigo,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: false,
-    src: sword_art,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "100%", width: "100%" },
-    },
-    header: "",
-    headerConfig: {},
-    title: "",
-    body: "",
-    footer: "",
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  }
 ];
 
 export { groupCardConfig, artData };
