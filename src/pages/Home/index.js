22bd8c48aca22f531c71fdf1f460eafc4619afa2
Naveen Prashanna G <35716708+Naveen015@users.noreply.github.com>
2023-08-30 21:20:38 -0500

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
-import abhishek from "@assets/abhishek.jpg";
+import naveen from "@assets/naveen.jpg";
 import { Container, Row, Col } from "react-bootstrap";
 import { Image, Paragraph, Title } from "@components/Components";
 import styled from "styled-components";
 import { aboutMe } from "./constants";
 
-import Abhishek_Rajendra_MSCS from "@assets/Abhishek_Rajendra_MSCS.pdf";
-import Abhishek_Rajendra_MSCS_ML from "@assets/Abhishek_Rajendra_MSCS_ML.pdf";
+import Naveen_CS from "@assets/Naveen_CS.pdf";
+import Naveen_ML from "@assets/Naveen_ML.pdf";
 
 const HomeContainer = styled.div`
   padding: 50px;
@@ -20,18 +20,18 @@ const Home = () => {
   return (
     <>
       <HomeContainer>
-        <Title>Welcome to Abhishek's Website!!</Title>
+        <Title>Welcome to Naveen's Website!!</Title>
         <Container>
           {/* Stack the columns on mobile by making one full-width and the other half-width */}
           <Row>
             <Col xs={12} md={8}>
               <Paragraph data={aboutMe}></Paragraph>
-              <a href={Abhishek_Rajendra_MSCS} target="_blank" rel="noreferrer">Resume for Software Engineer</a>
+              <a href={Naveen_CS} target="_blank" rel="noreferrer">Resume for Software Engineer</a>
               <br></br>
-              <a href={Abhishek_Rajendra_MSCS_ML} target="_blank" rel="noreferrer">Resume for Data Science/ML/Research</a>
+              <a href={Naveen_ML} target="_blank" rel="noreferrer">Resume for Data Science/ML/Research</a>
             </Col>
             <Col xs={6} md={4}>
-              <Image src={abhishek} {...options}></Image>
+              <Image src={naveen} {...options}></Image>
             </Col>
           </Row>       
         </Container>
