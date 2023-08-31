d344cdef7a6b3458264701a7d13d41da1500c28b
Naveen Prashanna G <35716708+Naveen015@users.noreply.github.com>
2023-08-31 11:17:07 -0500

    Create news constants.js

---
+++ src/pages/News/constants.js
@@ -0,0 +1,71 @@
+const tableConfig = {};
+
+const newsData = [
+  {
+    date: '<p style="color:red;">Aug 21, 2023</p>',
+    content: "Joined as Master Student at The University of Texas at Dallas",
+  },
+  {
+    date: '<p style="color:red;">Jul 4, 2022 - Jan 31, 2023</p>',
+    content: "Worked in Quantitative Brokers as Software Engineer",
+  },
+  {
+    date: '<p style="color:red;">Jul 13, 2022</p>',
+    content:
+      'Graduated from IIT Madras completing my Dual Degree in Mechanical Engineering.',
+  },
+  
+  {
+    date: '<p style="color:red;">Jul 13, 2022</p>',
+    content:
+      'Awarded a Minor Degree in Artificial Intelligence and Machine Learning from IIT Madras',
+  },
+  
+  {
+    date: '<p style="color:red;">Aug 2021 - May 2023</p>',
+    content:
+      'Completed Master’s Thesis on topic "Surface Texture Analysis" under guidance of <a href="https://home.iitm.ac.in/chalam/index.html"  target="_blank" rel="noreferrer">Prof. Dr. N. Arunachalam</a>',
+  },
+  {
+    date: '<p style="color:red;">Aug 2021 - Apr 2023</p>',
+    content:
+      'Teaching Assitant for Undergrad Course under <a href="https://home.iitm.ac.in/chalam/index.html"  target="_blank" rel="noreferrer">Prof. Dr. N. Arunachalam</a>',
+  },
+  {
+    date: '<p style="color:red;">May 2021 - Dec 2021</p>',
+    content:
+      "Worked in Quantitative Brokers as Software Engineer Intern",
+  }, 
+  {
+    date: '<p style="color:red;">May 2021</p>',
+    content:
+      "Received a Scholarship for Graduate Studies by securing merit score in GATE (Graduate Aptitude Test in Engineering)",
+  },
+  {
+    date: '<p style="color:red;">May 2021</p>',
+    content:
+      "",
+  },
+  {
+    date: '<p style="color:red;">Apr 2020 - Jun 2020</p>',
+    content:
+      "Worked in Big Data Science Research as a Software Engineer Intern",
+  },
+  {
+    date: '<p style="color:red;">Dec 2019</p>',
+    content:
+      "Awarded Silver prize in the ”Terrace Farming Robot for Hilly areas” challenge at Inter IIT Tech Meet",
+  },
+  {
+    date: '<p style="color:red;">May 2019 - Jun 2019</p>',
+    content:
+      "Worked in Alphabt - TVS Motors Ltd as a Machine Learning Engineer Intern",
+  },
+  {
+    date: '<p style="color:red;">Oct 2017</p>',
+    content:
+      "Secured a place in the Asia and Limca Book of Records for most number of robots cleaning an area ( A Clean India Initiative )",
+  },
+];
+
+export { newsData, tableConfig };
