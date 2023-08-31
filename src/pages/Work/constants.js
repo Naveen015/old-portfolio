71ce291491278bff95c3c65103f21c51c9372f3e
Naveen Prashanna G <35716708+Naveen015@users.noreply.github.com>
2023-08-31 10:27:16 -0500

    Update experience

--- src/pages/Work/constants.js
+++ src/pages/Work/constants.js
@@ -4,19 +4,18 @@ const workData = [
     isVideo: false,
     src: "",
     srcConfig: {},
-    header: "AirAsia",
+    header: "Quantitative Brokers",
     headerConfig: { as: "h5" },
     title: "Software Engineer",
     body:
-      "<ul><li>Introduced ETag feature in microservices using Spring Boot, helping to validate the cache in the mobile app; making it 20% faster and consume 40% lesser bandwidth.</li>" +
-      "<li>Created a Vanilla JavaScript to export React widgets using webpack to enable cross-sell capability across different Tech Stack (including React Native) and increased profits by 10% and reusability brought down the development time from weeks to hours.</li>" +
-      "<li>Created REST API microservice using Python to give dynamic recommendations for order of carousels on the homepage by recommendation model to give real-time relevant data to users.</li>" +
-      "<li>Created a Seamless and faster way to update/create data for homepage in Content Stack using REST API by taking data from google sheets for all languages saving resource-consumption.</li>" +
-      "<li>Build UI Automating testing for AirAsia Homepage covering 80% components." +
-      "<li><strong>Tools used:</strong> Spring Boot, Python, Java, React, React Native, Flask, Next.js, Google Cloud Platform, TestNG, Selenium, Airflow</li>" +
+      "<ul><li> Developed an internal tool to create, update & seamlessly transmit FIX (Financial Information eXchange) Order messages to the Trading Engine</li>"+
+      "<li>Enhanced the user experience of the FIX tool by on-boarding functionalities for Multi-Leg Orders placement which increased efficiency by 15%</li>"+
+      "<li>Strengthened code quality by integrating SonarQube and BlackDuck scanners into Jenkins Pipeline, leading to a 20% reduction in critical issues</li>"+
+      "<li>Developed a Full-Stack VueJS Web-App and Enhanced database efficiency by migrating to Decentralized PostgreSQL using ETL Script</li>"+
+      "<li><strong>Tools Used:</strong> Python, C++, VueJS, PostgreSQL</li>"+
       "</ul>",
     footer:
-      '<p style="margin:0;padding:0;">July 2020 - July 2021 <a href="https://www.airasia.com/en/gb"  target="_blank" rel="noreferrer">AirAsia</a></p>',
+      '<p style="margin:0;padding:0;">May 2021 - Jan 2023 <a href="https://quantitativebrokers.com/"  target="_blank" rel="noreferrer">Quantitative Brokers</a></p>',
     footerConfig: { className: "text-muted d-flex justify-content-end" },
   },
   {
@@ -24,16 +23,31 @@ const workData = [
     isVideo: false,
     src: "",
     srcConfig: {},
-    header: "Engimat Simulation Private Limited",
+    header: "Big Data Science Research",
     headerConfig: { as: "h5" },
     title: "Software Engineer Intern",
     body:
-      "<ul><li>Created a cloud application using Python-OpenCV towards feature extraction from 2D engineering drawings and reconstruction it to 3D CAD models in SCAD/STL format.</li>" +
-      "<li>Developed an Android Application to use the above cloud service and created a public library of 3D Models.</li>" +
-      "<li><strong>Tools used:</strong> Android Studio, Firebase, Python, OpenCV, OpenSCAD</li>" +
+      "<ul><li>Extracted Google Maps data via Selenium & replicated on OpenStreetMap using Folium, Convex Hull algorithm & Web Mercator Projection</li>" +
+      "<li>Devised a Map-matching algorithm to connect buildings to roads & Developed software to simulate traffic based on road & building density</li>" +
       "</ul>",
     footer:
-      '<p style="margin:0;padding:0;">May 2019 - July 2019 <a href="https://drive.google.com/file/d/1jB0ZSMDB2uGZArl8hvXV1G8fR1nySIQ8/view"  target="_blank" rel="noreferrer">Intership Certificate</a></p>',
+      '<p style="margin:0;padding:0;">Apr 2020 - Jun 2020 <a href="https://drive.google.com/file/d/1HEVI4NyJlgO8Na94ohQK8wQB8kWyODVE/view?usp=sharing"  target="_blank" rel="noreferrer">Intership Certificate</a></p>',
+    footerConfig: { className: "text-muted d-flex justify-content-end" },
+  },
+  {
+    cardConfig: {},
+    isVideo: false,
+    src: "",
+    srcConfig: {},
+    header: "Alphabt - TVS Motors Ltd",
+    headerConfig: { as: "h5" },
+    title: "Machine Learning Engineer Intern",
+    body:
+      "<ul><li>Implemented a Python program to scan the label on vehicles using openCV module which boosted the performance of validation system by 3%.</li>"+
+      "<li>Enhanced image processing accuracy by constructing a custom Object Detection model with TensorFlow to read engraved texts on vehicles.</li>"+
+      "</ul>",
+    footer:
+      '<p style="margin:0;padding:0;">May 2019 - Jun 2019 <a href="https://drive.google.com/file/d/1I1oqyTxd5xZbbPtf18-1jDj23iRU8C8A/view?usp=drive_link"  target="_blank" rel="noreferrer">Intership Certificate</a></p>',
     footerConfig: { className: "text-muted d-flex justify-content-end" },
   },
 ];
