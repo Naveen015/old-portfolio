9bed7f553b3fc5d0448fa4e593c639f82411d21c
Naveen Prashanna G <35716708+Naveen015@users.noreply.github.com>
2023-08-31 10:41:11 -0500

    Update Pages.js

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -2,8 +2,7 @@ import Home from "@pages/Home";
 import Work from "@pages/Work";
 import Projects from "@pages/Projects";
 import Publications from "@pages/Publications";
-import Achievements from "@pages/Achievements";
 import News from "@pages/News";
 import Hobbies from "@pages/Hobbies";
 
-export { Home, Work, Projects, Publications, Achievements, News, Hobbies };
+export { Home, Work, Projects, Publications, News, Hobbies };
