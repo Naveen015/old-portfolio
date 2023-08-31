e44162a4960e6c95685fe09ae57b0d745bd5e9b1
Naveen Prashanna G <35716708+Naveen015@users.noreply.github.com>
2023-08-31 11:32:18 -0500

    Update constants.js

--- src/pages/Publications/constants.js
+++ src/pages/Publications/constants.js
@@ -1,5 +1,4 @@
-import reconstruction from "@assets/Publication/reconstruction.png";
-import rl from "@assets/Publication/rl.mp4";
+import glcm from "@assets/Publication/methodology_glcm.jpg";
 
 const groupCardConfig = {
   xs: "1",
@@ -11,24 +10,7 @@ const publicationData = [
   {
     cardConfig: {},
     isVideo: false,
-    src: reconstruction,
-    srcConfig: {
-      variant: "top",
-      style: { display: "block", height: "15em", width: "100%" },
-    },
-    header: "arXiv",
-    headerConfig: { as: "h5" },
-    title: "Automated 3D solid reconstruction from 2D CAD using OpenCV",
-    body:
-      'Co-authors: <a href="https://www.ajaybharish.com/home"  target="_blank" rel="noreferrer">Ajay B Harish, University of California, Berkeley</s>',
-    footer:
-      '<p style="margin:0;padding:0;">2020 <a href="https://arxiv.org/pdf/2101.04248"  target="_blank" rel="noreferrer">arXiv</a> <a href="https://github.com/bhajay/ACADGen"  target="_blank" rel="noreferrer">github</a></p>',
-    footerConfig: { className: "text-muted d-flex justify-content-end" },
-  },
-  {
-    cardConfig: {},
-    isVideo: true,
-    src: rl,
+    src: glcm,
     srcConfig: {
       variant: "top",
       controls: true,
@@ -38,11 +20,11 @@ const publicationData = [
     },
     header: "Thesis",
     headerConfig: { as: "h5" },
-    title: "Analysis of Actor-Critic Algorithms And it’s Variants",
+    title: "Surface Texture Analysis",
     body:
-      'Guided by: <a href="https://scholar.google.co.uk/citations?user=JSL--pMAAAAJ&hl=en"  target="_blank" rel="noreferrer">Prabuchandran K.J</a>',
+      'Guided by: <a href="https://scholar.google.co.uk/citations?hl=en&user=V1cCMvUAAAAJ"  target="_blank" rel="noreferrer">Prof. Dr. N. Arunchalam</a>',
     footer:
-      '<p style="margin:0;padding:0;">2019 <a href="https://drive.google.com/file/d/18apC0A7_ddQt5dAXFz6Kg76u5eIKIY79/view?usp=sharing"  target="_blank" rel="noreferrer">paper</a> <a href="https://github.com/Abhishek-Rajendra/Actor_critic_methods"  target="_blank" rel="noreferrer">github</a></p>',
+      '<p style="margin:0;padding:0;">2022 <a href="https://drive.google.com/file/d/1BIkTqzbBxrTToCuS65CIrnZO1FTiik3g/view?usp=sharing"  target="_blank" rel="noreferrer">paper</a> <a href="https://github.com/Naveen015/Surface-Texture-Analysis"  target="_blank" rel="noreferrer">github</a></p>',
     footerConfig: { className: "text-muted d-flex justify-content-end" },
   },
 ];
